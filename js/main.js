9d54060cc7e6c208b47a4f75496dddcf057ddf0b
Shaiba123 <shaiba123@yandex.ru>
2021-07-18 23:41:37 +0300

    create: added swiper-slider in hotel section

--- js/main.js
+++ js/main.js
@@ -1,20 +1,9 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
   },
 });
